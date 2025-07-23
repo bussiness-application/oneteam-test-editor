eba4325e5f0cff70cfa8d801479ceb433a062863
ma-araki <ma-araki@psc-inc.co.jp>
2025-07-23 15:41:43 +0900

    ビルド出力先をデフォルトに修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // シングルページアプリケーション(SPA)を出力する
-  distDir: "build", // ビルド出力ディレクトリを'build'に変更
   images: {
     unoptimized: true, // 静的エクスポート用に画像最適化を無効化
     remotePatterns: [
